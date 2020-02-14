af3a5ed20a8c8c13c349684e571fd06a63535ab0
Leonardo Cardozo <leonardojc85@hotmail.com>
2020-02-13 23:45:11 -0300

    no message

---
+++ Proyecto_Consultorio_Medico/Negocios/HistorialNegocios.cs
@@ -0,0 +1,29 @@
+﻿using Proyecto_Consultorio_Medico.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Consultorio_Medico.Negocios
+{
+    class HistorialNegocios
+    {
+        HistorialConsultas historial = new HistorialConsultas();
+
+        public bool Save(HistorialConsultas historial)
+        {
+            return historial.Save(historial);
+        }
+
+        public ICollection<HistorialConsultas> Get()
+        {
+            return historial.Get();
+        }
+
+        public HistorialConsultas Get(int id)
+        {
+            return historial.Get(id);
+        }
+    }
+}
