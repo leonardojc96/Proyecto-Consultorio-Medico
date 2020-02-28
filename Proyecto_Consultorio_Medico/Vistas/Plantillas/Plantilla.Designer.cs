7adeaee1d5cfd2443788996d1b03983f9d33547c
Leonardo Cardozo <leonardojc85@hotmail.com>
2020-02-27 22:16:55 -0300

    limite de pantalla info

--- Proyecto_Consultorio_Medico/Vistas/Plantillas/Plantilla.Designer.cs
+++ Proyecto_Consultorio_Medico/Vistas/Plantillas/Plantilla.Designer.cs
@@ -55,7 +55,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 525);
             this.Controls.Add(this.panelHeader);
             this.Name = "Plantilla";
             this.Text = "Plantilla";
--- Proyecto_Consultorio_Medico/Vistas/Plantillas/PlantillaInfo.Designer.cs
+++ Proyecto_Consultorio_Medico/Vistas/Plantillas/PlantillaInfo.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.picBoxFoto = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.picBoxFoto)).BeginInit();
             this.SuspendLayout();
             // 
@@ -36,7 +37,7 @@
             // 
             this.panelHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(32)))), ((int)(((byte)(61)))));
             this.panelHeader.ForeColor = System.Drawing.Color.White;
-            this.panelHeader.Size = new System.Drawing.Size(1370, 55);
+            this.panelHeader.Size = new System.Drawing.Size(876, 55);
             // 
             // picBoxFoto
             // 
@@ -47,11 +48,21 @@
             this.picBoxFoto.TabIndex = 1;
             this.picBoxFoto.TabStop = false;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(789, 516);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // PlantillaInfo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1370, 892);
+            this.ClientSize = new System.Drawing.Size(876, 555);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.picBoxFoto);
             this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -60,6 +71,7 @@
             this.Load += new System.EventHandler(this.PlantillaInfo_Load);
             this.Controls.SetChildIndex(this.panelHeader, 0);
             this.Controls.SetChildIndex(this.picBoxFoto, 0);
+            this.Controls.SetChildIndex(this.button1, 0);
             ((System.ComponentModel.ISupportInitialize)(this.picBoxFoto)).EndInit();
             this.ResumeLayout(false);
 
@@ -68,5 +80,6 @@
         #endregion
 
         public System.Windows.Forms.PictureBox picBoxFoto;
+        private System.Windows.Forms.Button button1;
     }
 }
